6e14cd6eb92c994aa09b9f290142735a98cc901f
hayasitakahiro <0kv1td3.7254p9e@au.com>
2022-06-28 20:00:05 +0900

    コントローラーにリクエストを送信する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,17 @@
 class PostsController < ApplicationController
 
- def index
-  @posts = Post.order(id: "DESC")
- end
-
- # コメントアウトする
-  # def new
-  # end
-
+  def index
+    @posts = Post.order(id: "DESC")
+  end
+ 
+   # コメントアウトする
+   # def new
+   # end
+ 
   def create
     Post.create(content: params[:content])
     redirect_to action: :index  # 追記する
   end
  
-
-
-end
+ end
+ 
